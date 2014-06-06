b0c434445139fc3a807747fc14d9b34f9b4a2189
Erik van der Veen <erikvdv1@gmail.com>
2014-06-06 17:06:37 +0200

    Simple sorting view

--- app/scripts/controllers/display.js
+++ app/scripts/controllers/display.js
@@ -2,5 +2,31 @@
 
 angular.module('visualizerApp')
   .controller('DisplayController', ['$scope', 'jsonFactory', function ($scope, jsonFactory) {
-    $scope.data = jsonFactory.getData();
+    $scope.pullRequests = jsonFactory.getData();
+
+    $scope.sort = function sort (on) {
+      var data = jsonFactory.getData();
+      var func = function(a, b) {
+        a = on(a);
+        b = on(b);
+        if (a === b)
+          return 0;
+
+        return a > b ? 1 : -1;
+      };
+
+      $scope.pullRequests = data.sort(func);
+    };
+
+    $scope.onDate = function onDate (pr) {
+      return pr.createdAt;
+    };
+
+    $scope.onNumber = function onNumber (pr) {
+      return pr.number;
+    };
+
+    $scope.onNumberConflicts = function onNumberConflicts (pr) {
+      return pr.conflictsWith.length;
+    };
   }]);
