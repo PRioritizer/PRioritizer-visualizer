fa51abb19be97b7160a282c63062ae46b4a41e48
Erik van der Veen <erikvdv1@gmail.com>
2014-10-20 15:18:08 +0200

    Filter repos in controller to display repo count

--- app/scripts/controllers/explore.js
+++ app/scripts/controllers/explore.js
@@ -1,27 +1,35 @@
 'use strict';
 
 angular.module('visualizerApp')
-  .controller('ExploreController', ['$scope', '$upload', '$location', '$http', '$timeout', 'jsonFactory', function ($scope, $upload, $location, $http, $timeout, jsonFactory) {
+  .controller('ExploreController', ['$scope', '$upload', '$location', '$http', '$timeout', '$filter', 'jsonFactory', function ($scope, $upload, $location, $http, $timeout, $filter, jsonFactory) {
     $scope.fileApiSupport = jsonFactory.fileApiSupport;
     $scope.repos = [];
+    $scope.filteredRepos = [];
     $scope.perColumn = 0;
+    $scope.searchText = '';
 
     jsonFactory.init
       .success(function() {
         $scope.repos = jsonFactory.repositories.map(function(r) { return { owner: r.owner, repo: r.repo }; });
+        $scope.filteredRepos = $scope.repos;
         $scope.perColumn = Math.ceil($scope.repos.length / 3);
       });
 
     track();
+
+    /* Filter */
+    $scope.$watch('searchText', function (value) {
+      $scope.filteredRepos = $filter('filter')($scope.repos, {$:value});
+    });
 
     $scope.onFileClick = function onFileClick(repo) {
       $location.path('/display/' + repo.owner + '/' + repo.repo);
     };
 
     $scope.openProject = function openProject() {
-      $timeout(function() {
-        angular.element('.projects a').eq(0).trigger('click');
-      });
+      if ($scope.filteredRepos.length > 0) {
+        $scope.onFileClick($scope.filteredRepos[0]);
+      }
     };
 
     function track() {
