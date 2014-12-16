3e35dacff0994c22bf0ae987e9961c26420a2bed
Erik van der Veen <erikvdv1@gmail.com>
2014-12-16 14:16:54 +0100

    Redirect when only one search result

--- app/scripts/controllers/explore.js
+++ app/scripts/controllers/explore.js
@@ -13,6 +13,8 @@ angular.module('visualizerApp')
         $scope.repos = jsonFactory.repositories.map(function(r) { return { owner: r.owner, repo: r.repo, name: r.owner + '/' + r.repo }; });
         readParameters();
         filterRepos($scope.searchText);
+        if ($scope.filteredRepos.length === 1)
+          $scope.onFileClick($scope.filteredRepos[0]);
       });
 
     track();
@@ -22,7 +24,7 @@ angular.module('visualizerApp')
     $scope.$watch('searchText', filterRepos);
 
     $scope.onFileClick = function onFileClick(repo) {
-      $location.path('/display/' + repo.owner + '/' + repo.repo);
+      $location.url('/display/' + repo.owner + '/' + repo.repo);
     };
 
     $scope.openProject = function openProject() {
